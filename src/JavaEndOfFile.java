a6d7a1fea9b0210ac0def356a70203253ae1a3f0
Victor Martins <vmartins@fairfax.com.br>
2025-04-10 08:57:54 -0300

    Java End of File - solution

---
+++ src/JavaEndOfFile.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+// https://www.hackerrank.com/challenges/java-end-of-file/problem?isFullScreen=true
+// Categoria: Warmup
+// Linguagem: Java
+
+public class JavaEndOfFile {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 1;
+
+        while (sc.hasNext()) {
+            String line = sc.nextLine();
+            System.out.println(count + " " + line);
+            count++;
+        }
+    }
+}
