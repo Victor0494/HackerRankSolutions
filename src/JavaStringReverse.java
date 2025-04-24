d7a77e27fa475237d33a7da834ede6e9eac546f1
Victor Martins <victormartins0494@gmail.com>
2025-04-24 09:00:38 -0300

    Java String Reverse - Solution

---
+++ src/JavaStringReverse.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class JavaStringReverse {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String word=sc.next();
+        boolean palindrome = true;
+
+        for(int i = 0; i < word.length(); i++) {
+            if(word.charAt(i) != word.charAt(word.length()-i-1)) {
+                palindrome = false;
+                break;
+            }
+        }
+        System.out.println(palindrome ? "Yes": "No");
+    }
+}
